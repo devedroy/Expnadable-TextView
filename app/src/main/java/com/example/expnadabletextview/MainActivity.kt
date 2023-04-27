71ed3aec8e4b0da56aec248a9b9afe2e92e25c24
devedroy <devedroy.dr@gmail.com>
2023-04-27 21:51:01 +0530

    modelled

--- app/src/main/java/com/example/expnadabletextview/MainActivity.kt
+++ app/src/main/java/com/example/expnadabletextview/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.expnadabletextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
+import android.view.View
+import android.view.View.OnTouchListener
 import android.view.animation.AnimationUtils
 import com.example.expnadabletextview.databinding.ActivityMainBinding
 
@@ -14,9 +19,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-        binding.btn.setOnClickListener {
-            binding.tv.startAnimation(animation)
-        }
+        val zoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+        binding.tv.startAnimation(zoomIn)
+        binding.tv.setOnTouchListener(object : OnTouchListener {
+            private var scale = 1f
+            private val gestureDetector =
+                ScaleGestureDetector(this@MainActivity, object : SimpleOnScaleGestureListener() {
+                    override fun onScale(detector: ScaleGestureDetector): Boolean {
+                        scale *= detector.scaleFactor
+                        binding.tv.scaleX = scale
+                        binding.tv.scaleY = scale
+                        return true
+                    }
+
+                })
+
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                p1?.let { gestureDetector.onTouchEvent(it) }
+                return true
+            }
+
+        })
     }
 }
